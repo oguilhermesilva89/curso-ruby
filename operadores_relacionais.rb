5747a24c2ab70e0e164239051b95ba01ea497a43
oguilhermesilva89 <guilhermesallow@gmail.com>
2024-04-19 14:56:32 -0300

    Operadores relacionais

---
+++ operadores_relacionais.rb
@@ -0,0 +1,51 @@
+=begin
+    relacionais
+    >
+    >=
+    <
+    <=
+    ==
+    !=
+=end
+
+if 1 > 2
+    puts 'Sim'
+else 
+    puts 'Não'
+end
+# Não
+
+if 1 >= 2
+    puts 'Sim'
+else 
+    puts 'Não'
+end
+# Não
+
+if 1 < 2
+    puts 'Sim'
+else 
+    puts 'Não'
+end
+# Sim
+
+if 1 <= 2
+    puts 'Sim'
+else 
+    puts 'Não'
+end
+# Sim
+
+if 1 == 2
+    puts 'Sim'
+else 
+    puts 'Não'
+end
+# Não
+
+if 1 != 2
+    puts 'Sim'
+else 
+    puts 'Não'
+end
+# Sim
