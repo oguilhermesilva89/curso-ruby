5f62e22f3f52fe522066ce1d3efdea3b245645d2
oguilhermesilva89 <guilhermesallow@gmail.com>
2024-04-19 15:02:55 -0300

    Operadores aritméticos

---
+++ operadores_aritmeticos.rb
@@ -0,0 +1,16 @@
+=begin
+    Operadores Aritméticos
+    +
+    -
+    *
+    /
+    **
+    %
+=end
+
+puts "Soma de 10 + 10 = #{10 + 10}"
+puts "Subtração de 20 - 10 = #{20 - 10}"
+puts "Multiplicação de 10 * 10 = #{10 * 10}"
+puts "Divisao de 10 / 5 = #{10 / 5}"
+puts "Potencia de 10 ** 2 = #{10 ** 2}"
+puts "Resto de 10 % 2 = #{10 % 2}"
