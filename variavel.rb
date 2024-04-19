45e35661bb09ee6cf671362f21f4978916e3b815
oguilhermesilva89 <guilhermesallow@gmail.com>
2024-04-19 13:46:14 -0300

    Colocando o conteudo no arquivo

--- variavel.rb
+++ variavel.rb
@@ -1,1 +1,20 @@
 # Variáveis e versões do Ruby
+
+x = 1 
+# x.class =  Interger
+
+y = 2.4
+# y.class = Float
+
+z = "Guilherme"
+# z.class = String
+
+=begin Versões Ruby 
+    Algumas gems são compativés só até a 2.3 e outras a partir do 2.4
+        Exemplo: 
+        2.3
+            - Fixnum presenta os inteiros
+            - Bignum representa os números "inteiros gigantes"
+        2.4
+            - Integer representando todos os números inteiros
+=end
