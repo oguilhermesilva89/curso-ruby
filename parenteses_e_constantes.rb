cfc720591eb6ed422b2a22d9122eb7def0a8c3e4
oguilhermesilva89 <guilhermesallow@gmail.com>
2024-04-23 16:30:37 -0300

    Parênteses e Constantes

---
+++ parenteses_e_constantes.rb
@@ -0,0 +1,15 @@
+# No Ruby o uso de parênteses é opcional (usamos para facilitar a leitura)
+
+puts ("Curso de Rails - 1")
+puts "Curso de Rails - 2"
+
+puts "======================"
+# Constantes (São usadas para representar valores)
+# Se declara com letras maiuscula
+
+NOME = "Guilherme"
+puts NOME.class
+
+NOME = "Almeida" # Ele muda porém da uma mensagem avisando
+
+# Diferente das demais linguagens no Ruby a Constante pode ser alterada
