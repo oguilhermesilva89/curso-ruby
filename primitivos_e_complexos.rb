65ea672ed422a4893a364b2727ac345eca81a2db
oguilhermesilva89 <guilhermesallow@gmail.com>
2024-04-23 16:38:31 -0300

    Tipos primitivos e complexos

---
+++ primitivos_e_complexos.rb
@@ -0,0 +1,19 @@
+# Tipos Primitivos
+
+# Inteiro
+
+x = 1
+
+# Real / Float
+ y = 3.45
+
+ # Caracteres / Strings
+
+ z = 'abc'
+
+ # Lógico / Booleano
+
+ w = true
+ 
+
+ # Tipos Complexos (Classes e Objetos)
