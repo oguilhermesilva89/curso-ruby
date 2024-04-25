5a9a0c22de0cb7d46dadcb0d5081729983a760a0
oguilhermesilva89 <guilhermesallow@gmail.com>
2024-04-25 14:24:07 -0300

    Operadores de intervalo

---
+++ operadores_de_intervalo.rb
@@ -0,0 +1,14 @@
+# Operadores de intervalo
+
+#.. (Usar quando quero ir do primeiro elemento ao último elemento)
+
+(1..5).each do |i| 
+    puts i
+end
+
+puts "============="
+#...(Usar quando quero ir do primeiro elemento ao penúltimo elemento)
+
+(1...5).each do |i| 
+    puts i
+end
