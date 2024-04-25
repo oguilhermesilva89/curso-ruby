113c12f9dc1ad4507bcda982af2f99053158d478
oguilhermesilva89 <guilhermesallow@gmail.com>
2024-04-25 13:36:09 -0300

    Operadores de atribuição

---
+++ operadores_atribuicao.rb
@@ -0,0 +1,11 @@
+# Operadores de atribuição
+
+a = 2
+a += 4  # Igual à (a = a + 4)
+a -= 4  # Igual à (a = a - 4)
+a *= 4  # Igual à (a = a * 4)
+a /= 4  # Igual à (a = a / 4)
+a %= 4  # Igual à (a = a % 4)
+a **= 4 # Igual à (a = a ** 4)
+
+puts a
