3cda299be7850349f1e44c087a77f818f35fcaff
oguilhermesilva89 <guilhermesallow@gmail.com>
2024-04-30 17:08:33 -0300

    Method overriding

---
+++ poo/method_overriding.rb
@@ -0,0 +1,22 @@
+# Method overriding /  Método de sobrescrever
+
+class Calculadora
+    def somar(n1, n2)
+        n1 + n2
+    end
+end
+
+class CalculadoraFashion < Calculadora
+    # Overriding (Sobrescrita de método)
+    def somar(n1, n2)
+        "A soma é: #{n1 + n2}"
+    end
+end
+
+#########################################
+
+c = Calculadora.new
+puts c.somar(2,3)
+
+cf = CalculadoraFashion.new
+puts cf.somar(2,3)
